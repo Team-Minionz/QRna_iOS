d495e0f06e407e6436beb893065f16bf2bb86ce1
MangDic <remge007@naver.com>
2021-10-13 16:44:43 +0900

    feat. 로그인 / 회원가입 페이지 구성

--- QRna/QRna/UI/CustomButton.swift
+++ QRna/QRna/UI/CustomButton.swift
@@ -8,11 +8,35 @@
 import Foundation
 import UIKit
 
-class CustomButton : UIButton{
+class CustomButton : UIButton {
     required init(coder aDecoder: NSCoder){
         super.init(coder: aDecoder)!
         
         let colorLiteral = #colorLiteral(red: 0.4315146208, green: 0.5936716199, blue: 0.9271910191, alpha: 1)
+        self.layer.cornerRadius = 8
+        self.layer.backgroundColor = colorLiteral.cgColor
+        self.setTitleColor(.white, for: .normal)
+        
+    }
+}
+
+class SignInButton : UIButton {
+    required init(coder aDecoder: NSCoder){
+        super.init(coder: aDecoder)!
+        
+        let colorLiteral = #colorLiteral(red: 0.8799410462, green: 0.2819583118, blue: 0.2278155088, alpha: 1)
+        self.layer.cornerRadius = 8
+        self.layer.backgroundColor = colorLiteral.cgColor
+        self.setTitleColor(.white, for: .normal)
+        
+    }
+}
+
+class CancelButton : UIButton {
+    required init(coder aDecoder: NSCoder){
+        super.init(coder: aDecoder)!
+        
+        let colorLiteral = #colorLiteral(red: 0.3607542515, green: 0.3608012795, blue: 0.3607382178, alpha: 1)
         self.layer.cornerRadius = 8
         self.layer.backgroundColor = colorLiteral.cgColor
         self.setTitleColor(.white, for: .normal)
